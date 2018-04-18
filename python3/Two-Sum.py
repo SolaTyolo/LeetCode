68753ae1ecc6dc6c976c9772696465bbaa41e927
SolaTyolo <solat.yolo@gmail.com>
2018-04-18 18:52:29 +0800

    add python3 two-sum solution2

--- python3/Two-Sum.py
+++ python3/Two-Sum.py
@@ -14,7 +14,8 @@
 #  
 # 纯暴力超时 T-T         
 # Time Limit Exceeded
-#               
+# 
+# 时间复杂度O(n^2)
 #####################
 class Solution:
     def twoSum(self, nums, target):
@@ -42,8 +43,9 @@ class Solution:
 #   做个转化成value2 = target - value1
 #   在判断value2是否在列表中即可去掉一层循环
 #
-# Accepted  Runtime: 1632 ms
+# Accepted  Runtime: 1080 ms
 #
+# 时间复杂度O(n)
 ############################################
 class Solution1(object):
     def twoSum(self, nums, target):
@@ -60,12 +62,42 @@ class Solution1(object):
                 index2 = nums.index(value2)
                 if index1 != index2:
                     key_list = [index1,index2]
+                    break
         return sorted(key_list)
+
+##############################################
+#
+# Tip:
+#   上述方法耗时超过了1s
+#   而当nums的列表较大时nums.index()就比较耗时了
+#   转化个思想把每次的num及索引存在一个集合中(哈希表)
+#   这个每次查询 target-num 时都是子集合查询
+#   
+# Accepted  Runtime: 36 ms
+#
+# 时间复杂度O(n)
+############################################
+
+
+class Solution2(object):
+    def twoSum(self, nums, target):
+        # """
+        # :type nums: List[int]
+        # :type target: int
+        # :rtype: List[int]
+        # """
+
+        hashtable = {}
+        for i, num in enumerate(nums):
+            if target-num in hashtable:
+                return [hashtable[target-num], i]
+            hashtable[num] = i
+
 # test
 def main():
-    nums = [3,-3]
-    target = 0
-    sol = Solution1()
+    nums = [2, 3,4,2,7, 11, 15]
+    target = 9
+    sol = Solution2()
     res = sol.twoSum(nums, target)
     print(res)
 
